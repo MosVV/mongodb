2af10787937a6ac196b0b4683c2ae0846bc1a5b3
MosVV <vv-mosolov@rambler.ru>
2025-10-25 14:51:39 +0400

    Add delelete is mongodb

--- src/main/java/com/example/mongo/user/controller/UserApiController.java
+++ src/main/java/com/example/mongo/user/controller/UserApiController.java
@@ -12,6 +12,7 @@ import com.example.mongo.user.routes.UserRoutes;
 import lombok.AllArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -72,17 +73,25 @@ public class UserApiController {
     }
 
     @PutMapping(UserRoutes.BY_ID)
-    public UserResponse edit(@PathVariable String id, @RequestBody EditUserRequest request) throws ObjectIdParseException,UserNotFoundException{
-        if(!ObjectId.isValid(id))throw new ObjectIdParseException();
+    public UserResponse edit(@PathVariable String id, @RequestBody EditUserRequest request) throws ObjectIdParseException, UserNotFoundException {
+        if (!ObjectId.isValid(id)) throw new ObjectIdParseException();
 
-        UserDoc userDoc=userRepository
+        UserDoc userDoc = userRepository
                 .findById(new ObjectId(id))
                 .orElseThrow(UserNotFoundException::new);
 
         userDoc.setLastName(request.getLastName());
         userDoc.setFirstName(request.getFirstName());
 
-        userDoc=userRepository.save(userDoc);
+        userDoc = userRepository.save(userDoc);
         return UserResponse.of(userDoc);
+    }
+
+    @DeleteMapping(UserRoutes.BY_ID)
+    public String  delete(@PathVariable String id) throws ObjectIdParseException {
+        if (!ObjectId.isValid(id)) throw new ObjectIdParseException();
+
+        userRepository.deleteById(new ObjectId(id));
+        return HttpStatus.OK.name();
     }
 }
